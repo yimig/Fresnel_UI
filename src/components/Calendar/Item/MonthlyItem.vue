f9d54dd160795bb6d0013e5948ba980191d982fe
yimig <zhangyiming@programmer.net>
2022-05-17 15:54:29 +0800

    add month calendar item

--- src/components/Calendar/Item/MonthlyItem.vue
+++ src/components/Calendar/Item/MonthlyItem.vue
@@ -1,10 +1,31 @@
 <template>
-
+<div :class="is_current_month ? 'h-full w-full bg-blue-light' : 'h-full w-full bg-gray-light'" >
+  <div class="text-xl text-lightdark ml-2 mt-0.5">{{current_day}}</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'MonthlyItem'
+  name: 'MonthlyItem',
+  props: ['pos', 'year', 'month'],
+  data () {
+    return {
+      current_day: this.getDay().getDate(),
+      current_month: this.getDay().getMonth(),
+      current_year: this.getDay().getFullYear(),
+      is_current_month: this.getDay().getMonth() === Number(this.month) - 1
+    }
+  },
+  methods: {
+    getWeekDay: function (year, month, day) {
+      const date = new Date(year, month, day)
+      return date.getDay() === 0 ? 7 : date.getDay()
+    },
+    getDay: function () {
+      const gap = this.getWeekDay(Number(this.year), Number(this.month) - 1, 1) - 1
+      return new Date(Number(this.year), Number(this.month) - 1, Number(this.pos) - gap)
+    }
+  }
 }
 </script>
 
--- src/components/Calendar/Monthly.vue
+++ src/components/Calendar/Monthly.vue
@@ -10,49 +10,27 @@
       <div class="title" id="week7">星期日</div>
     </div>
     <div class="grid grid-cols-7 flex-grow">
-      <div class="flex day" id="day1"></div>
-      <div class="flex day" id="day2"></div>
-      <div class="flex day" id="day3"></div>
-      <div class="flex day" id="day4"></div>
-      <div class="flex day" id="day5"></div>
-      <div class="flex day" id="day6"></div>
-      <div class="flex day" id="day7"></div>
-      <div class="flex day" id="day8"></div>
-      <div class="flex day" id="day9"></div>
-      <div class="flex day" id="day10"></div>
-      <div class="flex day" id="day11"></div>
-      <div class="flex day" id="day12"></div>
-      <div class="flex day" id="day13"></div>
-      <div class="flex day" id="day14"></div>
-      <div class="flex day" id="day15"></div>
-      <div class="flex day" id="day16"></div>
-      <div class="flex day" id="day17"></div>
-      <div class="flex day" id="day18"></div>
-      <div class="flex day" id="day19"></div>
-      <div class="flex day" id="day20"></div>
-      <div class="flex day" id="day21"></div>
-      <div class="flex day" id="day22"></div>
-      <div class="flex day" id="day23"></div>
-      <div class="flex day" id="day24"></div>
-      <div class="flex day" id="day25"></div>
-      <div class="flex day" id="day26"></div>
-      <div class="flex day" id="day27"></div>
-      <div class="flex day" id="day28"></div>
-      <div class="flex day" id="day29"></div>
-      <div class="flex day" id="day30"></div>
-      <div class="flex day" id="day31"></div>
-      <div class="flex day" id="day32"></div>
-      <div class="flex day" id="day33"></div>
-      <div class="flex day" id="day34"></div>
-      <div class="flex day" id="day35"></div>
+<!--      .flex.day#day$*42>MonthlyItem[:pos=++start_pos][:year=year][:month=month]-->
+      <div v-for="(item,i) in new Array(item_number)" class="flex day" :id="'day' + (i + 1)">
+        <MonthlyItem :pos="i + 1" :year="year" :month="month"></MonthlyItem>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import MonthlyItem from './Item/MonthlyItem.vue'
 export default {
   name: 'Monthly',
   components: {
+    MonthlyItem
+  },
+  data () {
+    return {
+      year: 2022,
+      month: 5,
+      item_number: 7 * 6
+    }
   }
 }
 </script>
